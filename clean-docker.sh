188189ac2c24d77a1e0ee2d4064f8aeff1061f74
jslahti <jslahti@gmail.com>
2023-02-01 16:25:56 +0200

    scriptien päivitystä

---
+++ clean-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LOGFILE="/home/fituser/logs/log.log"
+ScriptDirectory="/home/fituser/digitransit-ui-fitme/"
+
+cd $ScriptDirectory
+
+/bin/bash /home/fituser/digitransit-ui-fitme/build-dt.sh
+
+echo "$(date "+%m%d%Y %T") : build script done"
+
+/bin/bash /home/fituser/digitransit-ui-fitme/run-dt.sh
+
+echo "$(date "+%m%d%Y %T") : run script done"
+echo "$(date "+%m%d%Y %T") : All ok"

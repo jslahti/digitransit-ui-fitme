88dc31174833fc685253056fbb6099cf98b168ad
Kinnunen Timo <Timo.Kinnunen@vtt.fi>
2024-02-15 16:06:59 +0200

    Fixed a bug in PoiStore PART 3.

--- app/store/PoiStore.js
+++ app/store/PoiStore.js
@@ -38,47 +38,23 @@ class PoiStore extends Store {
     const newpois = [];
     let isSame = false;
     console.log(['setPoiPoints pois=',pois,' viapoints=',viapoints]);
-    if (this.poiPoints.length > 0) {
-      // Store has old POIs and viapoints are defined
-      // If POI is included in viapoints array => keep it.
-      if (viapoints.length > 0) {
-        viapoints.forEach(vp=>{
-          this.poiPoints.every(oldpoi=>{
-            // If POIs exist => we go through the list and check which ones to keep.
-            // NOTE: viapoints do NOT have index, only address, lat and lon
-            if (vp.lat === oldpoi.lat && vp.lon === oldpoi.lon) {
-              newpois.push(oldpoi);
-              return false; // break out from the .every loop
-            }
-            return true; // continue with next oldpoi
-          });
-        });
-      }
-    }
-    if (pois.length > 0) {
-      if (this.poiPoints.length > 0) {
-        if (this.isEqual(pois, this.poiPoints)) {
-          isSame = true;
-        }
-      }
-      if (!isSame) {
-        pois.forEach(poi=>{
-          newpois.push(poi);
-        });
+    // Check if the POI SETS are same
+    if (pois.length > 0 && this.poiPoints.length > 0) {
+      if (this.isEqual(pois, this.poiPoints)) {
+        isSame = true;
       }
     }
     if (isSame) {
       console.log('=== PoiStore is NOT changed! ==='); 
     } else {
-      if (newpois > 0) {
-        this.poiPoints = newpois;
-        console.log(['CHANGES TO Store this.poiPoints=',this.poiPoints,'and EMIT CHANGE!']);
-        this.emitChange();
-      } else {
-        this.poiPoints = [];
-        console.log('=== EMPTY the Store and EMIT change! ===');
-        this.emitChange();
+      if (pois.length > 0) {
+        pois.forEach(poi=>{
+          newpois.push(poi);
+        });
       }
+      this.poiPoints = newpois;
+      console.log(['CHANGES TO Store this.poiPoints=',this.poiPoints,'and EMIT CHANGE!']);
+      this.emitChange();
     }
   }
 
